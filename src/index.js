ec2807121e2e2293a2ffed14e5fa7dcd4937c177
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-19 15:52:36 -0300

    Add proxy to forward API calls

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import 'babel-polyfill';
 import express from 'express';
+import proxy from 'express-http-proxy';
 import { matchRoutes } from 'react-router-config';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 import routes from './client/routes';
 
 const app = express();
+
+app.use(
+  '/api',
+  proxy('https://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+      // eslint-disable-next-line no-param-reassign
+      opts.headers['x-forwarded-host'] = 'localhost:3000';
+      return opts;
+    },
+  }),
+);
 
 app.use(express.static('public'));
 
@@ -22,5 +34,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Running');
+  console.log('Running application server...');
 });
