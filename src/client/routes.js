5e9d5c4c20eb7095005720526a9a030bb0ccff4b
Frederico Martins Porto <martinsporto@gmail.com>
2019-02-19 22:22:55 -0300

    Add not found page

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,7 +1,15 @@
 import App from './App';
 import HomePage from './pages/HomePage';
 import UserList from './pages/UserListPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 export default [
-  { ...App, routes: [{ ...HomePage, path: '/', exact: true }, { ...UserList, path: '/users' }] },
+  {
+    ...App,
+    routes: [
+      { ...HomePage, path: '/', exact: true },
+      { ...UserList, path: '/users' },
+      { ...NotFoundPage },
+    ],
+  },
 ];
